f0dec01086f1841e920b786bb82a7c4895ca881e
kaori kohama <kkaori131415@gmail.com>
2020-07-08 18:30:41 +0900

    display all currencies in option tag

--- app.js
+++ app.js
@@ -4,9 +4,26 @@ new Vue({
     currencies: {}
   },
   mounted(){
-    axios.get("http://free.currencyconverterapi.com/api/v6/currencies?apiKey=bb59a43164ca670b9c14")
-    .then(response => {
+    this.getCurrencies()
+  },
+  computed:{
+    formattedCurrencies(){
+      return Object.values(this.currencies)
+    }
+  },
+  methods: {
+    getCurrencies() {
+      const currencies = localStorage.getItem("currencies")
+
+      if(currencies){
+        this.currencies = JSON.parse(currencies)
+        return;
+      }
+      axios.get("http://free.currencyconverterapi.com/api/v6/currencies?apiKey=bb59a43164ca670b9c14")
+      .then(response => {
       this.currencies = response.data.results;
-    })
+      localStorage.setItem("currencies", JSON.stringify(response.data.results))
+      })
+    }
   }
 })
