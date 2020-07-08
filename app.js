4ee093fef5407f0647949bce4ddcc2cf303baa6d
kaori kohama <kkaori131415@gmail.com>
2020-07-08 19:27:43 +0900

    converting currencies with api

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ new Vue({
     currencies: {},
     amount: null,
     from: "EUR",
-    to: "USD"
+    to: "USD",
+    api: "bb59a43164ca670b9c14",
+    result: 0
   },
   mounted(){
     this.getCurrencies()
@@ -12,6 +14,9 @@ new Vue({
   computed:{
     formattedCurrencies(){
       return Object.values(this.currencies)
+    },
+    culculateResult(){
+      return (Number(this.amount) * this.result).toFixed(2);
     }
   },
   methods: {
@@ -22,10 +27,18 @@ new Vue({
         this.currencies = JSON.parse(currencies)
         return;
       }
-      axios.get("http://free.currencyconverterapi.com/api/v6/currencies?apiKey=bb59a43164ca670b9c14")
+      axios.get(`http://free.currencyconverterapi.com/api/v6/currencies?apiKey=${this.api}`)
       .then(response => {
       this.currencies = response.data.results;
       localStorage.setItem("currencies", JSON.stringify(response.data.results))
+      })
+    },
+    convertCurrency(){
+      const key = `${this.from}_${this.to}`
+      axios.get(`http://free.currencyconverterapi.com/api/v6/convert?q=${key}&compact=ultra&apiKey=${this.api}`)
+      .then(response => {
+        console.log(response)
+        this.result = response.data[key]
       })
     }
   }
